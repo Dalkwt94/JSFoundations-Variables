40fa75deaa40a11ad59dfe0071c6d420044031e4
Dee Macbook <deemacbook@Dees-MacBook-Pro.local>
2020-06-15 18:55:18 +0300

    solved variables task

--- story.js
+++ story.js
@@ -1,7 +1,11 @@
-// Define the name here on line 2
+const name = "Dalal";
 
-// Define the age here on line 4
+const age = 25;
 
-// Define the hobby here on line 6
+const hobby = "painting";
 
-// Print the story here on line 8
+console.log(
+  `Mrs. ${name} , in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
